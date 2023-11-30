52cea0b361dd7cdad98f21acac936cb5d7b2c91d
Shal Malhotra <smalhotra0109@gmail.com>
2023-11-30 18:38:02 -0500

    Update helpers.py

--- lib/helpers.py
+++ lib/helpers.py
@@ -6,8 +6,8 @@ from models.civilization import Civilization
 from time import sleep
 from sys import stdout
 
-def helper_1():
-    print("Performing useful function#1.")
+# def helper_1():
+#     print("Performing useful function#1.")
 
 
 def exit_program():
@@ -26,3 +26,13 @@ def initialize_from_database():
     Planet.get_all()
     Species.get_all()
     Civilization.get_all()
+    
+def list_stars():
+    stars = Star.get_all()
+    for star in stars:
+        print(star)
+
+# def find_star_by_type():
+#     type = input("Enter the star type:")
+#     star = Star.find_by_type(type)
+#     print(star) if star else print(f"Star {type} not found")
