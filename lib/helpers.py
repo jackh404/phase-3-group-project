aa3e38500d0edc136020d3e840a23642692a78bd
Jack Henza <jackhenza@gmail.com>
2023-11-30 21:26:54 -0500

    more ui methods

--- lib/helpers.py
+++ lib/helpers.py
@@ -83,6 +83,11 @@ def list_species():
     for species in species :
         scan_print(f'{species.id}: {species.name}')
         
+def list_civilizations():
+    civs = Civilization.get_all()
+    for civ in civs :
+        scan_print(f'{civ.id}: {civ.name}')
+        
 def find_planet_by_type():
     type = input("Enter the type of planet: ")
     planet = Planet.find_by_type(type)
@@ -155,7 +160,45 @@ def create_planet():
                 planet = None
     print()
     input("Press Enter to return to menu")
+    print()
     
+def create_species():
+    species = None
+    name = None
+    type = None
+    description = None
+    home_world_id = None
+    while(not species):
+        if not name: name = input("Enter the name of the new species: ")
+        print()
+        scan_print("Available species types:")
+        list_types(Species)
+        print()
+        if not type: type = input("Enter the type number of the new species: ")
+        type = Species.types[int(type)-1]
+        if not description: description = input("Enter the description of the new species: ")
+        print()
+        scan_print("Available planets:")
+        list_planets()
+        print()
+        if not home_world_id: home_world_id = input("Enter the home world ID of the new species: ")
+        try:
+            species = Species.create(name, type, description, int(home_world_id))
+            scan_print(f"Species {name} created successfully!\n{species}")
+        except Exception as e:
+            scan_print(f"Error: {e}")
+            if input("Try again? (y/n): ") == "n":
+                break
+            else:
+                if "Name" in e.__str__(): name = None
+                elif "Type" in e.__str__(): type = None
+                elif "Description" in e.__str__(): description = None
+                elif "Home World" in e.__str__(): home_world_id = None
+                species = None
+    print()
+    input("Press Enter to return to menu")
+    print()
+
 def create_civilization():
     civ = None
     name = None
@@ -221,3 +264,75 @@ def create_civilization():
                 
     print()
     input("Press Enter to return to menu")
+    print()
+    
+def update_civilization():
+    list_civilizations()
+    id_ = input("Enter the civilization's id: ")
+    try:
+        if civ := Civilization.find_by_id(int(id_)):
+            print(civ)
+            ans = input("Change the civilization's name? (y/n): ")
+            if "y" in ans.lower():
+                name = input("Enter the civilizations's new name: ")
+                civ.name = name
+            ans = input("Change the civilizations's type? (y/n): ")
+            if "y" in ans.lower():
+                print()
+                scan_print("Available civilization types:")
+                list_types(Civilization)
+                print()
+                input("Enter the new type number for the civilization: ")
+                type = Civilization.types[int(type)-1]
+                civ.type = type
+            ans = input("Change the civilizations's description? (y/n): ")
+            if "y" in ans.lower():
+                description = input("Enter the new description: ")
+                civ.description = description
+            ans = input("Change the civilizations's major religions? (y/n): ")
+            if "y" in ans.lower():
+                religions = input("Enter the new major religions separated by commas: ")
+                civ.religions = religions
+            ans = input("Change the civilizations's major languages? (y/n): ")
+            if "y" in ans.lower():
+                languages = input("Enter the new major languages separated by commas: ")
+                civ.languages = languages
+            ans = input("Change the civilizations's species? You will need to enter the ID of each species again. (y/n): ")
+            if "y" in ans.lower():
+                civ.species_ids = []
+                while True:
+                    print()
+                    scan_print("Available Species:")
+                    list_species()
+                    print()
+                    id_ = input("Enter the ID of a species in the civilization or press Enter to continue: ")
+                    if id_:
+                        id_ = int(id_)
+                        if id_ not in civ.species_ids:
+                            civ.species_ids.append(id_)
+                    else:
+                        break
+            ans = input("Change the civilizations's planets? You will need to enter the ID of each planet again. (y/n): ")
+            if "y" in ans.lower():
+                civ.species_ids = []
+                while True:
+                    print()
+                    scan_print("Available Planets:")
+                    list_planets()
+                    print()
+                    id_ = input("Enter the ID of a planet in the civilization or press Enter to continue: ")
+                    if id_:
+                        id_ = int(id_)
+                        if id_ not in civ.planet_ids:
+                            civ.planet_ids.append(id_)
+                    else:
+                        break
+            civ.update()
+            scan_print(f"Civilization {civ.name} updated successfully!\n{civ}")
+        else:
+            print(f"Civilization {id_} not found")
+    except Exception as exc:
+        print("Error updating: ", exc)
+    print()
+    input("Press Enter to return to menu")
+    print()
