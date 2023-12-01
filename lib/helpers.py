e47c2e2ece635824e3dfd46752fca29551a2ee48
Jack Henza <jackhenza@gmail.com>
2023-11-30 20:08:37 -0500

    create planet ui method

--- lib/helpers.py
+++ lib/helpers.py
@@ -30,21 +30,62 @@ def initialize_from_database():
 def list_stars():
     stars = Star.get_all()
     for star in stars:
-        print(star)
+        scan_print(f'{star.id}: {star.name}')
 
 def find_star_by_type():
     type = input("Enter the star type:")
     star = Star.find_by_type(type)
     print(star) if star else print(f"Star {type} not found")
 
-def find
-
-
+def list_types(cls):
+    for i, t in enumerate(cls.types):
+        scan_print(f'{i+1}: {t}')
 
 
 def create_planet():
-    pname = input("Enter the name of the new planet: ")
-    ptype = input("Enter the type of the new planet: ")
-    pdescription = input("Enter the description of the new planet: ")
-    pdiameter = input("Enter the diameter of the new planet in kilometers:")
-    
+    planet = None
+    pname = None
+    ptype = None
+    pdescription = None
+    pdiameter = None
+    pmass = None
+    pday = None
+    pyear = None
+    pstar = None
+    while(not planet):
+        if not pname: pname = input("Enter the name of the new planet: ")
+        print()
+        scan_print("Available planet types:")
+        list_types(Planet)
+        print()
+        if not ptype: ptype = input("Enter the type number of the new planet: ")
+        ptype = Planet.types[int(ptype)-1]
+        if not pdescription: pdescription = input("Enter the description of the new planet: ")
+        if not pdiameter: pdiameter = input("Enter the diameter of the new planet in kilometers: ")
+        if not pmass: pmass = input("Enter the mass of the new planet in trillions of kilograms: ")
+        if not pday: pday = input("Enter the day length of the new planet in Earth days: ")
+        if not pyear: pyear = input("Enter the year length of the new planet in Earth years: ")
+        print()
+        scan_print("Available stars:")
+        list_stars()
+        print()
+        pstar = input("Enter the star ID of the new planet: ")
+        try:
+            planet = Planet.create(pname, ptype, pdescription, pdiameter, pmass, pday, pyear, int(pstar))
+            scan_print(f"Planet {pname} created successfully!\n{planet}")
+        except Exception as e:
+            scan_print(f"Error: {e}")
+            if input("Try again? (y/n): ") == "n":
+                break
+            else:
+                if "Name" in e.__str__(): pname = None
+                elif "Type" in e.__str__(): ptype = None
+                elif "Description" in e.__str__(): pdescription = None
+                elif "Diameter" in e.__str__(): pdiameter = None
+                elif "Mass" in e.__str__(): pmass = None
+                elif "Day" in e.__str__(): pday = None
+                elif "Year" in e.__str__(): pyear = None
+                elif "Star" in e.__str__(): pstar = None
+                planet = None
+    print()
+    input("Press Enter to return to menu")
